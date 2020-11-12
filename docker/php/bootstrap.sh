d00988cbeec3150ae38f62b87375820be2378326
Michel Chowanski <michel@chowanski.de>
2020-11-12 20:41:50 +0100

    Fix error in bootstrap file

--- docker/php/bootstrap.sh
+++ docker/php/bootstrap.sh
@@ -3,13 +3,15 @@
 ENVIRONMENT=$1
 PHP_INI_DIR=$2
 
-if [ $ENVIRONMENT == 'production' ]
+if [[ $ENVIRONMENT == 'production' ]]
 then
+  echo "Bootstrap application for production ..."
   mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
   cp ./docker/php/development.ini $PHP_INI_DIR/conf.d/
 
   composer install --no-dev --no-interaction --optimize-autoloader
 else
+  echo "Bootstrap application for development ..."
   mv "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
   cp ./docker/php/development.ini $PHP_INI_DIR/conf.d/
 fi
